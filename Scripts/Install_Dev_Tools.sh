2a548db62ea846ae53a06a5059eeafa90a0d101f
Patrick W <my_mak@web.de>
2023-11-14 21:08:42 +0100

    added MonoGame Template to DevTools Install Script

--- Scripts/Install_Dev_Tools.sh
+++ Scripts/Install_Dev_Tools.sh
@@ -5,10 +5,9 @@ echo "Installing Dev Tools"
 sudo pacman -Syyu
 sudo pacman -S krita blender gimp cmake github-cli dotnet-runtime dotnet-sdk aspnet-runtime mono android-udev mono-msbuild mono-msbuild-sdkresolver
 
-
-
 yay -Syyu
 yay -S rider godot-mono-bin github-desktop-bin visual-studio-code-bin mono-basic android-sdk ncurses5-compat-libs aseprite-git
 
+dotnet new install "MonoGame.Templates.CSharp"
 
 echo "Installation finished. Please reboot now !!!"
