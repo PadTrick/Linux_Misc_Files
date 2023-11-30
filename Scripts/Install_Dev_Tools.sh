a45f90de09e4f925e12cc91c87fe44a73cfc20c5
Patrick W <my_mak@web.de>
2023-11-30 14:35:52 +0100

    Update Install_Dev_Tools.sh

--- Scripts/Install_Dev_Tools.sh
+++ Scripts/Install_Dev_Tools.sh
@@ -6,7 +6,7 @@ sudo pacman -Syyu
 sudo pacman -S krita blender gimp cmake github-cli dotnet-runtime dotnet-sdk aspnet-runtime mono android-udev mono-msbuild mono-msbuild-sdkresolver
 
 yay -Syyu
-yay -S rider godot-mono-bin github-desktop-bin visual-studio-code-bin mono-basic android-sdk ncurses5-compat-libs aseprite-git
+yay -S rider godot-mono-bin github-desktop-bin visual-studio-code-bin mono-basic android-sdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-sdk-cmake android-platform android-ndk xamarin-android ncurses5-compat-libs aseprite-git
 
 dotnet new install "MonoGame.Templates.CSharp"
 
