2330090d7f2967663c54ce2f4343cd382e9bdecc
Patrick W <my_mak@web.de>
2023-12-18 08:21:04 +0100

    removed rider, added directx-git and neofetch

--- Scripts/Install_Dev_Tools.sh
+++ Scripts/Install_Dev_Tools.sh
@@ -6,8 +6,12 @@ sudo pacman -Syyu
 sudo pacman -S krita blender gimp cmake github-cli dotnet-runtime dotnet-sdk dotnet-targeting-pack dotnet-targeting-pack-6.0 dotnet-host aspnet-runtime mono android-udev mono-msbuild mono-msbuild-sdkresolver
 
 yay -Syyu
-yay -S rider godot-mono-bin github-desktop-bin visual-studio-code-bin mono-basic android-sdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-sdk-cmake android-platform android-ndk xamarin-android ncurses5-compat-libs aseprite-git
+yay -S godot-mono-bin github-desktop-bin visual-studio-code-bin mono-basic android-sdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-sdk-cmake android-platform android-ndk xamarin-android ncurses5-compat-libs aseprite-git
 
 dotnet new install "MonoGame.Templates.CSharp"
+dotnet new install "Avalonia.Templates"
+
+echo "See https://github.com/dotnet/templating/wiki/Available-templates-for-dotnet-new for more"
+
 
 echo "Installation finished. Please reboot now !!!"
--- Scripts/Install_IntelArc_Driver.sh
+++ Scripts/Install_IntelArc_Driver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-yay -S intel-graphics-compiler-bin intel-compute-runtime-bin mesa-git lib32-mesa-git vulkan-headers vulkan-validation-layers vulkan-man-pages vulkan-tools libva-intel-driver libvdpau-va-gl libva-utils intel-ucode intel-media-driver linux-firmware
+yay -S intel-graphics-compiler-bin intel-compute-runtime-bin mesa-git lib32-mesa-git vulkan-headers vulkan-validation-layers vulkan-man-pages vulkan-tools libva-intel-driver libvdpau-va-gl libva-utils intel-ucode intel-media-driver linux-firmware directx-headers-git
 echo "All Finished !!!"
