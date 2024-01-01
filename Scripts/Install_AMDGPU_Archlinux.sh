ffed213e80e2adae7f3bccdc0ca9106929b30573
Patrick W <my_mak@web.de>
2024-01-01 23:49:10 +0100

    Update Install_AMDGPU_Archlinux.sh

--- Scripts/Install_AMDGPU_Archlinux.sh
+++ Scripts/Install_AMDGPU_Archlinux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo pacman mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
+sudo pacman -S mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
 echo "AMD Driver installed. Plz reboot !!!"
