972a6f48906b6244330598b675ee59a2f27bb6e2
Patrick W <my_mak@web.de>
2024-01-10 23:06:04 +0100

    updated Intel Arc Driver Package (GIT version)

--- Scripts/Install_IntelArc_Driver_GIT.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-yay -S intel-graphics-compiler-git intel-compute-runtime-bin mesa-git lib32-mesa-git vulkan-headers-git vulkan-validation-layers-git vulkan-man-pages vulkan-tools-git lib32-vulkan-tools libva-intel-driver-git libvdpau-va-gl lib32-libvdpau-va-gl libva-utils-git intel-ucode-git intel-media-driver-git lib32-intel-media-driver linux-firmware directx-headers-git mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver vulkan-mesa-layers lib32-vulkan-mesa-layers opencl-clover-mesa lib32-opencl-clover-mesa
-echo "All Finished !!!"
