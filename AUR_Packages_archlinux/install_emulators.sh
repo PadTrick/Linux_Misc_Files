eb3b35437f0caa2c8ed56a16f35d49046e891efe
Patrick W <my_mak@web.de>
2024-06-07 19:53:22 +0200

    Add files via upload

---
+++ AUR_Packages_archlinux/install_emulators.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yay -S desmume fceux melonds mgba-qt ryujinx-git snes9x-gtk mupen64plus simple64 duckstation-git ppsspp pcsx2 fs-uae fs-uae-launcher
+echo "All installations finished !!!"
