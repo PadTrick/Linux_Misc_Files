4ddf16393faf006be2a18186ceedbf810b27d426
Patrick W <my_mak@web.de>
2024-06-08 09:39:38 +0200

    small changes

--- AUR_Packages_archlinux/install_emulators.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yay -S desmume fceux melonds mgba-qt ryujinx-git snes9x-gtk mupen64plus simple64 duckstation-git ppsspp pcsx2 fs-uae fs-uae-launcher
-echo "All installations finished !!!"
