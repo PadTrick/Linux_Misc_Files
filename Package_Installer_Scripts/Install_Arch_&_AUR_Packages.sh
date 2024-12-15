d8bfad08214d16b978cde3dfc01e7b9fde5c6b8f
Patrick W <my_mak@web.de>
2024-12-15 02:44:08 +0100

    removed firefox, added vivaldi

--- Package_Installer_Scripts/Install_Arch_&_AUR_Packages.sh
+++ Package_Installer_Scripts/Install_Arch_&_AUR_Packages.sh
@@ -232,7 +232,7 @@ sudo pacman -S cpupower --noconfirm
 sudo pacman -S vlc --noconfirm
 
 #browsers
-sudo pacman -S firefox --noconfirm
+sudo pacman -S vivaldi --noconfirm
 
 #gaming overlay
 sudo pacman -S mangohud lib32-mangohud goverlay --noconfirm
