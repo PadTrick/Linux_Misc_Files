c2de06affe4bd082c61a72db4128cc81d00986d7
Patrick W. <my_mak@web.de>
2025-02-18 10:28:05 +0100

    added WebStorm & Aqua

--- Package_Installer_Scripts/Install_Dev_Tools_without_aseprite.sh
+++ Package_Installer_Scripts/Install_Dev_Tools_without_aseprite.sh
@@ -6,7 +6,7 @@ sudo pacman -Syyu
 sudo pacman -S krita blender gimp cmake github-cli dotnet-runtime dotnet-sdk dotnet-targeting-pack dotnet-targeting-pack-6.0 dotnet-host aspnet-runtime mono android-udev mono-msbuild mono-msbuild-sdkresolver --noconfirm
 
 yay -Syyu
-yay -S github-desktop-bin gnome-keyring visual-studio-code-bin mono-basic android-sdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-sdk-cmake android-platform android-ndk xamarin-android ncurses5-compat-libs pycharm-community-jre laigter rider rustrover --noconfirm
+yay -S github-desktop-bin gnome-keyring visual-studio-code-bin mono-basic android-sdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-sdk-cmake android-platform android-ndk xamarin-android ncurses5-compat-libs pycharm-community-jre laigter rider rustrover webstorm aqua --noconfirm
 
 dotnet new install "MonoGame.Templates.CSharp"
 dotnet new install "Avalonia.Templates"
@@ -16,3 +16,9 @@ echo "See https://github.com/dotnet/templating/wiki/Available-templates-for-dotn
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo "Installation finished. Please reboot now !!!"
+echo "======================================================"
+echo "To fix runtime errors with RustRover,WebStorm and Aqua"
+echo "goto /opt/appname and start the app at the welcome screen"
+echo "press CTRL+SHIFT+A and type >Choose Boot Java Runtime for the IDE ...<"
+echo "Select default and press >OK<, now it should work."
+echo "======================================================"
