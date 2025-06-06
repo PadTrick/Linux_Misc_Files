38716310c8ab363c1774fc9d988d31e67f075c90
Patrick W. <my_mak@web.de>
2025-06-06 13:27:08 +0200

    cleanup

---
+++ Post_Archinstall_Script/Copy_Fonts_archlinux.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+WORKING_DIR=$(pwd)
+sudo mkdir /usr/local/share/fonts
+sudo mkdir /usr/local/share/fonts/otf
+sudo mkdir /usr/local/share/fonts/ttf
+sudo cp -r $WORKING_DIR/otf /usr/local/share/fonts
+sudo cp -r $WORKING_DIR/ttf /usr/local/share/fonts
+
+
+sudo pacman -S noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-dejavu ttf-liberation ttf-opensans --noconfirm
+echo "Fonts have been installed !!!"
