bc47e9c7696ba0d1ff3fbdd2ab4654e8cdbf327f
Patrick W. <my_mak@web.de>
2025-10-24 13:08:44 +0200

    added phpstorm to dev script

--- Package_Installer_Scripts/Install_Dev_Tools_with_asesprite.sh
+++ Package_Installer_Scripts/Install_Dev_Tools_with_asesprite.sh
@@ -8,7 +8,7 @@ sudo pacman -S godot krita blender gimp cmake github-cli --noconfirm
 #sudo pacman -S dotnet-runtime dotnet-sdk dotnet-targeting-pack dotnet-targeting-pack-6.0 dotnet-host aspnet-runtime mono android-udev mono-msbuild mono-msbuild-sdkresolver --noconfirm
 
 yay -Syyu
-yay -S gnome-keyring visual-studio-code-bin pycharm-community-jre laigter rider rustrover webstorm material-maker-bin blockbench-bin aseprite --noconfirm
+yay -S gnome-keyring visual-studio-code-bin pycharm-community-jre laigter rider rustrover webstorm material-maker-bin blockbench-bin phpstorm aseprite --noconfirm
 #Disabled C# & Android Stuff
 #yay -S github-desktop-bin mono-basic android-sdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-sdk-cmake android-platform android-ndk ncurses5-compat-libs --noconfirm
 
@@ -27,7 +27,7 @@ rustup target add i686-unknown-linux-gnu
 
 echo "Installation finished. Please reboot now !!!"
 echo "======================================================"
-echo "To fix runtime errors with RustRover and WebStorm"
+echo "To fix runtime errors with PHPStorm, RustRover and WebStorm"
 echo "goto /opt/appname and start the app at the welcome screen"
 echo "press CTRL+SHIFT+A and type >Choose Boot Java Runtime for the IDE ...<"
 echo "Select default and press >OK<, now it should work."
