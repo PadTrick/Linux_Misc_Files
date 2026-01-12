69b79631f6d05997b45002f7ebe8ca8128c5b6f3
Patrick W. <my_mak@web.de>
2026-01-12 03:54:05 +0100

    fixed some packages

--- Package_Installer_Scripts/Install_IntelArc_Driver.sh
+++ Package_Installer_Scripts/Install_IntelArc_Driver.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo pacman -Syu
 sudo pacman -S intel-graphics-compiler intel-compute-runtime mesa lib32-mesa vulkan-headers vulkan-validation-layers vulkan-tools libva-intel-driver libvdpau-va-gl libva-utils intel-ucode intel-media-driver linux-firmware directx-headers libva-mesa-driver lib32-libva-mesa-driver vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-driver lib32-vulkan-driver vulkan-mesa-implicit-layers lib32-vulkan-mesa-implicit-layers
 echo "All Finished !!!"
 
